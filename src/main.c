5cf5841fdd9ac32db8d5e45fd929a5b5e3472b8a
Alex Tabisz <alex@tabisz.org>
2026-04-20 11:02:24 +1000

    Add --help / -h flag with usage examples

--- src/main.c
+++ src/main.c
@@ -20,9 +20,30 @@ static void process(FILE *f) {
     }
 }
 
+static void print_help(void) {
+    puts("Usage: jive [OPTION] [FILE]");
+    puts("       command | jive");
+    puts("");
+    puts("Translate text into jive dialect.");
+    puts("Reads from FILE if given, otherwise from standard input.");
+    puts("");
+    puts("Options:");
+    puts("  -h, --help    Show this help and exit");
+    puts("");
+    puts("Examples:");
+    puts("  echo \"What are you doing?\" | jive");
+    puts("  jive readme.txt");
+    puts("  jive < input.txt > output.txt");
+    puts("  git log --oneline | jive");
+}
+
 int main(int argc, char *argv[]) {
     srand((unsigned int)time(NULL));
     if (argc >= 2) {
+        if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+            print_help();
+            return 0;
+        }
         FILE *f = fopen(argv[1], "r");
         if (!f) {
             fprintf(stderr, "jive: cannot open '%s'\n", argv[1]);
